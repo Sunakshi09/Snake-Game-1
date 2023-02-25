cfc260472c66a1013b1ddb4c37a5ae54702e6d17
sunaks <sunakshithapar@gmail.com>
2023-02-25 11:49:45 -0500

    wall collision

--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ const gameOverSound = new Audio("gameover.mp3");
 const foodSound = new Audio("food.mp3");
 const moveSound = new Audio("move.mp3");
 const musicSound = new Audio("musicsound.mp3");
-let speed = 6;
+let speed = 4;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 11, y: 15 }];
-food = { x: 6, y: 7 };
+
+food = { x: 7, y: 8 };
 
 // Game Functions //
 function main(ctime) {
@@ -19,7 +20,25 @@ function main(ctime) {
   lastPaintTime = ctime;
   gameEngine();
 }
-function isCollision(snakeArr) {
+
+//if snake bump into itself
+function isCollision(snake) {
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+  }
+
+  //if snake hit by the wall
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
+
   return false;
 }
 
@@ -29,7 +48,7 @@ function gameEngine() {
     gameOverSound.play();
     musicSound.pause();
     inputDir = { x: 0, y: 0 };
-    alert("Game Over. Press shift key to play again!");
+    alert("Game Over.Press Shift key to play again!");
     snakeArr = [{ x: 11, y: 15 }];
     musicSound.play();
     score = 0;
@@ -38,6 +57,8 @@ function gameEngine() {
   // If my snake eaten the food, what you have to do next, increment the score and regenerate the foodd
   if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
     foodSound.play();
+    score += 1;
+    score, (innerHTML = "Score: " + score);
     snakeArr.unshift({
       x: snakeArr[0].x + inputDir.x,
       y: snakeArr[0].y + inputDir.y,
@@ -54,28 +75,28 @@ function gameEngine() {
   for (let i = snakeArr.length - 2; i >= 0; i--) {
     snakeArr[i + 1] = { ...snakeArr[i] };
   }
-
   snakeArr[0].x += inputDir.x;
   snakeArr[0].y += inputDir.y;
 
   // Step 2: I want display the Snake & the Food
-  // Step 3: I want display the Snake
+  //  I want display the Snake
   board.innerHTML = "";
   snakeArr.forEach((e, index) => {
     snakeElement = document.createElement("div");
-    snakeElement.style.gridRowstart = e.y;
-    snakeElement.style.gridColumnstart = e.x;
+    snakeElement.style.gridRowStart = e.y;
+    snakeElement.style.gridColumnStart = e.x;
+
     if (index === 0) {
       snakeElement.classList.add("head");
     } else {
-      snakeElement.classList.add("snake");
+      snakeElement.classList.add("food");
     }
     board.appendChild(snakeElement);
   });
   //display the food
   foodElement = document.createElement("div");
-  foodElement.style.gridRowstart = food.y;
-  foodElement.style.gridColumnstart = food.x;
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
   foodElement.classList.add("food");
   board.appendChild(foodElement);
 }
@@ -90,26 +111,25 @@ window.addEventListener("keydown", (e) => {
       console.log("ArrowUp");
       inputDir.x = 0;
       inputDir.y = -1;
-
       break;
 
     case "ArrowDown":
       console.log("ArrowDown");
       inputDir.x = 0;
       inputDir.y = 1;
-
       break;
+
     case "ArrowLeft":
       console.log("ArrowLeft");
       inputDir.x = -1;
       inputDir.y = 0;
       break;
+
     case "ArrowRight":
       console.log("ArrowRight");
       inputDir.x = 1;
       inputDir.y = 0;
       break;
-
     default:
       break;
   }
